d06abe00933c08feb9df7e9d304658575f3b4091
mehedi-hasan-oni <mehedi018255@gmail.com>
2022-01-28 01:51:19 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+//Testimonial-slider start
+
+$('.servic-slider').slick({
+  dots: false,
+  prevArrow:'<i class="fas fa-long-arrow-alt-left prev"></i>',
+  nextArrow:'<i class="fas fa-long-arrow-alt-right next"></i>', 
+  infinite: true,
+  initialSlide:0,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+});
+
+
+//proatflo _mixit up js start 
+var mixer = mixitup('.portflo-imge');
+
+
+//proatflo _venobox js start 
+$(document).ready(function(){
+    $('.venobox').venobox(); 
+});
+
+
+//Testimonial-slider start
+
+$('.Testimonial-slider').slick({
+  dots: false,
+  prevArrow:'<i class="fas fa-long-arrow-alt-left prev"></i>',
+  nextArrow:'<i class="fas fa-long-arrow-alt-right next"></i>', 
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+});
+
+//Testimonial-slider start
+
+
